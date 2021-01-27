c2bcd4ae41a887710376de2f494803e7360e27d9
jraleman <jraleman@bendiburg.com>
2021-01-27 02:27:16 -0800

    Adds generate-resume script idea

---
+++ generate-resume.sh
@@ -0,0 +1,27 @@
+# generate-resume.sh
+
+# by default, exclude the following
+    ## about
+    ## awards
+    ## languages
+# this is to beconsistent to what we
+# print on the cv website page
+
+# via flags
+# -e, --exclude
+# -a, --all (don't exclude anything)
+# --senior-software-engineer
+# --mobile-developer
+# --mechatronics-engineer
+# --frontend-engineer
+# --fullstack-engineer
+
+# by default, exclude the following
+    ## skills
+        ### interpersonal
+        ### other skills
+    ## education
+        ### AMP
+        ### PAS
+    ## experience
+        ### electricidad-total
